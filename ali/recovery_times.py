745bc9c1ca97af0c3c5217ccf9bb0dec07a0b72e
proscrite <pablogfr94@gmail.com>
2020-03-24 16:48:18 +0100

    Added wet/dry, area and pumping fit analysis methods

--- ali/recovery_times.py
+++ ali/recovery_times.py
@@ -18,7 +18,7 @@ def filterRepeatedOccurrences(a : np.array, th: int = 10) -> np.array:
     b = np.where(a[-1:0:-1] - a[-2::-1] > 10)[0]       # Since we iterated backwards, the resulting b array is also in the reverse order, so we flip it
     return np.flip(a[len(a)-1 - b])
 
-def findRecoveryPoints(pChamber : np.array, pAct : float) -> np.array:
+def findRecoveryPoints(pChamber : np.array, pAct : float, th : int = 10) -> np.array:
     """Compute recovery time until a specified pressure level is reached
     Input:
     pChamber : np.array
@@ -30,7 +30,7 @@ def findRecoveryPoints(pChamber : np.array, pAct : float) -> np.array:
         recovery point indices for the whole timeline"""
 
     a = np.where(pChamber < pAct)[0]
-    a = filterRepeatedOccurrences(a)
+    a = filterRepeatedOccurrences(a, th = th)
     return a
 
 def computeRecoveryTimes(recPoints : np.array, scale : str = 's') -> np.array:
@@ -77,7 +77,8 @@ def peakRecoveryTimes(pChamber : np.array, pAct : float) -> np.array:
     else :
         return b/100
 
-def plotRecoveryTimes(dfp : pd.DataFrame, pAct : float , ax : plt.axes = plt.gca()) -> list:
+def plotRecoveryTimes(dfp : pd.DataFrame, pAct : float , ax : plt.axes = plt.gca(),
+                     col : str = 'b', lb : str = '') -> list:
     """Compute and plot recovery times from a dfpPeak and specified pAct
     Return recovery time
     Input:
@@ -88,18 +89,41 @@ def plotRecoveryTimes(dfp : pd.DataFrame, pAct : float , ax : plt.axes = plt.gca
     """
     rT = []
     Npeaks = len(dfp.columns)
-    for i in range(Npeaks):
-        rPt = findRecoveryPoints(dfp['peak'+str(i)], pAct)
+    for i in dfp.columns:
+        rPt = findRecoveryPoints(dfp[i], pAct)
         if len(rPt) > 0:
             rT.append(rPt[0]/100)
     if ax == None:
         ax = plt.gca()
-    ax.plot(rT, 'o')
-    ax.set_xlabel('peak nr.')
+    ax.plot(rT, 'o', color=col, label=lb)
+    ax.set_xlabel('pulse nr.')
     ax.set_ylabel('recovery time [s]')
     return rT
 
-def plot_maxP(dfp : pd.DataFrame, ax : plt.axes = plt.gca()) -> list:
+def shorten_dfp(dfp : pd.DataFrame, pThres : float, inTh : int = 5):
+    """Crop dfp of ALI pulse up to time to recover pThres pressure
+    Input:
+    ---------------
+    dfp : ALI pulses df
+    pThres : pressure level to recover
+    inTh : minimum number of index points
+        for recPoints to be separated
+
+    Returns:
+    dfShort : shortened dfp
+    ---------------
+    """
+    rP = []
+    dfShort = pd.DataFrame()
+    Npeaks = len(dfp.columns)
+    for i in dfp:
+        rPt = findRecoveryPoints(dfp[i], pAct=pThres, th=inTh)
+        if rPt.any():
+            rP.append(rPt[0])
+            dfShort[i] = dfp[i][:rPt[0]]
+    return dfShort
+
+def plot_maxP(dfp : pd.DataFrame, ax = None, flag_p : bool = False, col = 'b', lb : str = '') -> list:
     """Compute and plot max reached pressures in a dfPeak
     Return list of maxP
     Input:
@@ -110,30 +134,134 @@ def plot_maxP(dfp : pd.DataFrame, ax : plt.axes = plt.gca()) -> list:
         List of max. pressures reached per peak"""
 
     maxP = dfp.max(axis=0).values
-    ax.semilogy(maxP, 'o')
-
-    ax.set_xlabel('peak nr.')
-    ax.set_ylabel('$p_{max}$ [mbar]')
+    if flag_p:
+        if ax == None: ax = plt.gca()
+        ax.semilogy(maxP, 'o', color = col, label=lb)
+        ax.set_xlabel('pulse nr.')
+        ax.set_ylabel('$p_{max}$ [mbar]')
     return maxP
 
-def depletionFromMaxP(dfp) -> int:
-    """Find liquid exhaustion moment by fitting a constant to the maxP
-    scatter and choosing the maximum value of chi2. Plot fit and Chi2"""
-
+def split_wet_dry(dfp : pd.DataFrame, nsigma : float = 1., DEBUG : bool = False)->tuple:
+    """
+        Split run by chi2 soft thresholding
+        Input:
+        ------
+        dfp : pulse df to split
+        nsigma : nr of std deviations over/below average to set chi2 threshold
+            if negative reverse dry <-> wet
+        Output:
+        -------
+        dryPulses : dfp with lower p_max (bkg)
+        wetPulses : dfp with higher p_max
+    """
     maxP = plot_maxP(dfp)
     x = np.arange(len(maxP))
-    UmaxP = 0.3 * np.array(maxP)
+    UmaxP = 0.3 * np.array(maxP)         # Systematic error of press gauge
+
     from scipy.optimize import curve_fit
-    def func (x, a): return a
+    def const (x, a): return a          # fit to constant value to extract chi2
 
-    fit, cov = curve_fit(func,x, maxP)#, sigma=UmaxP, absolute_sigma=True)
-    sd = np.std(maxP)
-    # sd = cov[0]
+    fit, cov = curve_fit(const,x, maxP)
+    assert fit[0] > np.min(maxP) and fit[0] < np.max(maxP), "av maxP out of bounds"
+
     chi2 = (maxP - fit[0])**2/UmaxP**2
+    sd = np.std(chi2)
+    avg = np.average(chi2)
 
-    plt.axhline(y=fit[0], color='r')
-    plt.plot(x, chi2, '.')
-    # plt.fill_between(x, fit[0]-cov[0], fit[0]+cov[0], color='g', alpha=0.5)
-    plt.fill_between(x, fit[0]-sd, fit[0]+sd, color='r', alpha=0.3)
-    plt.title('Max pressure per pulse')
-    return np.argmax(chi2)
+    if DEBUG:
+        plt.axhline(y=avg, color='r')
+        plt.plot(x, chi2, '.')
+        plt.fill_between(x, avg - nsigma*sd, avg + nsigma*sd, color='r', alpha=0.3)
+        plt.title('Max pressure per pulse')
+
+    wetId = np.where(chi2 < avg + nsigma * sd)[0]
+    dryId = np.where(chi2 >= avg + nsigma * sd)[0]
+
+    wetPulses = pd.concat((dfp.iloc[:,p] for p in wetId), axis=1)
+    dryPulses = pd.concat((dfp.iloc[:,p] for p in dryId), axis=1)
+    return wetPulses, dryPulses
+
+def table_vnm_pulses(Ntot : int, Nwet : int,
+                     C : float, M_ba : float, Vload : float = 0):
+    """
+    Tabulate Volume, Number of molecules and mass injected per load, pulse
+    and wet pulse (assuming all solution injected only in wet pulse)
+    Input:
+    Ntot : total number of pulses
+    Nwet : total number of wet pulses
+    C : solution concentration (specify units)
+        example: C = 0.5 * milimole / liter # example
+    M_ba : molecular mass [specify units]
+        example: M_ba = 336.24 * gram/mol # molec mass [g/mol] for Ba(ClO4)2
+    Vload : volume of ALI load (default 0.3 ml)
+    """
+    from invisible_cities.core.system_of_units import ml, liter
+    from invisible_cities.core.system_of_units import milimole, mol, gram, milligram
+    NA = 6.023e23 # Avogadro constant [molec/mol]
+
+    if Vload == 0: Vload = 0.3 * ml
+
+    vols = (Vload / ml, Vload / Ntot / ml, Vload / Nwet / ml)
+    Nmolecs = tuple([v * C / milimole * NA for v in vols])
+    masses = tuple([v * C / milimole * M_ba / gram for v in vols])
+
+    print(' \t total load \t per pulse \t per wet pulse')
+    print('V [mL]\t %.2e \t %.2e \t %.2e' %(vols))
+    print('N \t %.2e \t %.2e \t %.2e' %(Nmolecs))
+    print('m [g] \t %.2e \t %.2e \t %.2e' %(masses))
+
+### Area integration, leak rate and leaked mass
+
+def area_raise(dfp : pd.DataFrame) -> list:
+    """Numerical integration of pulses until array maximum (peak)"""
+    # For one pulse:
+    # np.trapz(dfp.pulse0.iloc[:dfp.pulse0.idxmax()], x=dfp.pulse0.index[:dfp.pulse0.idxmax()]/100 )
+    upAreas = [np.trapz(dfp[p].iloc[:dfp[p].idxmax()], x=dfp[p].index[:dfp[p].idxmax()]/100 ) for p in dfp]
+    return upAreas
+
+def avLeakMass (wetP : pd.DataFrame(),
+                M : float = 41.05,
+                Seff : float = 0.25) :
+    """Integrate area under ALI pressure curves and compute leak rate and leaked mass.
+        Parameters
+        ----------
+        wetP : ALI pandas.Dataframe (wet pulses)
+        M : solution/gas molarity
+        Seff : pumping effective speed. Default value 0.25 for C0 = 0.26
+        Returns
+        ----------
+        leak_mass : average leaked mass in gramm
+        ql : average leak rates per pulse [mbar*L/s]
+    """
+
+    logtime = 0.01 # [10 ms]
+    T = 25+273 # Pulse valve temperature [K]
+    R = 83.144598 # Gas constant [L⋅mbar⋅K−1⋅mol−1]
+
+    avWet, sdWet, _ = avCurves(wetP, )
+    wetArea = area_raise(wetP)
+    dt = np.argmax(avWet)/100
+
+    ql = np.average(wetArea) * Seff
+    leak_mass = ql * dt * M / (R*T)
+    return leak_mass, ql
+
+def fit_pump_profile(dfp : pd.DataFrame(), xdown : list, pdown : list):
+    """Fit custom model to average pumping profile of ALI curve
+    Model is the sum of two exponential with different decay and a negative power of t
+    Input:
+    --------
+    dfp : ALI curve to extract the average profile
+    xdown : array of descend time to perform fit on
+    pdown : array of descend pressure to perform fit on"""
+
+    plotAverageProfile(dfp, lb='Average wet profile')
+
+    from scipy.optimize import curve_fit
+    def exp2(x, p0, tau, p1, n, p2, beta):
+        return p0*np.exp(-tau*(x)) + p1*(x)**n + p2*np.exp(-beta*x)
+
+    fitDw, covDw = curve_fit(exp2, xdown, pdown, p0=[pdown[0], 30, pdown[-1], 0.1, 5e-3, 0.5])
+    plt.semilogy(xdown, exp2(xdown, *fitDw), '-r', label='Fit model')
+    plt.legend()
+    return fitDw, #np.sqrt(np.diag(covDw))
