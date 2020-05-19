f77f45c4f04b2b6b58f1171c3ecfe1dae6f3fcf2
Pablo <pablogfr94@gmail.com>
2020-05-19 20:01:18 +0200

    4f system alignment nb

--- scripts/galvo_python/gaussian2dFit.py
+++ scripts/galvo_python/gaussian2dFit.py
@@ -28,6 +28,19 @@ def guess_centroid(im : np.ndarray):
 def select_roi(im : np.ndarray, bounds : list) -> np.ndarray:
     return im[bounds[0] : bounds[1], bounds[2] : bounds[3]]
 
+def plot_fit_result(im : np.ndarray, p0 : list, ax = None):
+    """Plot gaussian center (p0[1], p0[2]) as a cross
+    and a circle of radius p0[3]"""
+    if ax == None : ax = plt.gca()
+    pl = ax.imshow(im)#, cmap=plt.get_cmap('gray'))
+    cross = ax.scatter(x=p0[1], y=p0[2], marker='x',
+     color='r', label = '(%.1f, %.1f)' %(p0[1], p0[2]))
+
+    circle = plt.Circle((p0[1], p0[2]), radius=p0[3], color='r', lw=1, fill=False,
+     label = '$(\sigma_x, \sigma_y) = $ (%.1f, %.1f)' %(p0[3], p0[4]))
+    ax.add_artist(circle)
+    ax.legend([cross], ['(%.1f, %.1f)' %(p0[1], p0[2])])
+
 def fitGauss2d(im : np.ndarray, p0 : list):
     x = np.arange(0, im.shape[1], 1)
     y = np.arange(0, im.shape[0], 1)
@@ -61,21 +74,24 @@ def fitGauss2d(im : np.ndarray, p0 : list):
     fit = minimize(residuals, initial, args=(xx, yy, im))#, method='cg')
     return fit
 
-def make_params(fit):
+def make_params(fit, DEBUG : bool = False):
     par = []
     for k in fit.params.keys():
         par.append(fit.params[k].value)
-        print("{} = {}".format(k, fit.params[k].value))
+        if DEBUG:
+            print("{} = {}".format(k, fit.params[k].value))
     return par
+
+def gauss(x, *p):
+    A, mu, sigma, B = p
+    return A *  np.exp(-( x-mu )**2 / (2.*sigma**2)) + B
 
 def fitGausRowCol(im : np.ndarray, mx : tuple, flag_p : bool = False):
     """Fit a gaussian on the (row, column) of im specified by mx
     Can be used to guess sigma_x and sigma_y"""
-    def gauss(x, *p):
-        A, mu, sigma, B = p
-        return A *  np.exp(-( x-mu )**2 / (2.*sigma**2)) + B
+
     from peakutils import baseline
-    bl = np.average(baseline(imroi[mxroi[0], :]))
+    bl = np.average(baseline(im[mx[0], :]))
     from scipy.optimize import curve_fit
     yrow = im[mx[0], :]
     xrow = range(len(yrow))
@@ -111,44 +127,45 @@ def zoom_out(pars : list, bounds : list):
     newp[1:3] = newc
     return newp
 
-    def fit2dGauss(im : np.ndarray, DEBUG : bool = False) :
-        """Fit 2d gaussian to an RGB image, perform ROI selection, parameter setting"""
-        img = rgb2gray(im)
-        mx = guess_centroid(img)
-        bounds = bound_roi(img, mx, 500)
-        imroi = select_roi(img, bounds)
+def guessNfitGaussian2d(im : np.ndarray, DEBUG : bool = False, flag_p : bool = True) :
+    """Fit 2d gaussian to an RGB image, perform ROI selection, parameter setting"""
+    img = rgb2gray(im)
+    mx = guess_centroid(img)
+    bounds = bound_roi(img, mx, 500)
+    print(mx, bounds)
+    imroi = select_roi(img, bounds)
 
-        mxroi = guess_centroid(imroi)
-        flp = False
-        if DEBUG:
-            flp = True
-            pl = plt.imshow(imroi)#, cmap=plt.get_cmap('gray'))
-            plt.scatter(mx[1] - bounds[2], mx[0] - bounds[0], marker='x', color='r')
+    mxroi = guess_centroid(imroi)
+    flp = False
+    if DEBUG:
+        flp = True
+        pl = plt.imshow(imroi)#, cmap=plt.get_cmap('gray'))
+        plt.scatter(mx[1] - bounds[2], mx[0] - bounds[0], marker='x', color='r')
 
-        # Guess sigmas
+    # Guess sigmas
 
-        fitr, fitc = fitGausRowCol(imroi, mxroi, flag_p=flp)
-        sigma_x = fitc[2]
-        sigma_y = fitr[2]
+    fitr, fitc = fitGausRowCol(imroi, mxroi, flag_p=flp)
+    sigma_x = fitc[2]
+    sigma_y = fitr[2]
 
-        p0 = [imroi.max(), mxroi[1], mxroi[0], sigma_x, sigma_y]
-        if DEBUG:
-            plot_fit_result(imroi, p0)
-            plt.title('Initial parameter guess')
+    p0 = [imroi.max(), mxroi[1], mxroi[0], sigma_x, sigma_y]
+    if DEBUG:
+        plot_fit_result(imroi, p0)
+        plt.title('Initial parameter guess')
 
-        # Fit to 2dGaussian
+    # Fit to 2dGaussian
 
-        fit = fitGauss2d(imroi, p0 = p0)
+    fit = fitGauss2d(imroi, p0 = p0)
 
-        pars = make_params(fit)
-        if DEBUG:
-            plot_fit_result(imroi, pars)
-            plt.title('Fit result')
+    pars = make_params(fit)
+    if DEBUG:
+        plot_fit_result(imroi, pars)
+        plt.title('Fit result')
 
-        #  Zoom out of ROI
+    #  Zoom out of ROI
 
-        newp = zoom_out(pars, bounds)
-        if DEBUG:
-            plot_fit_result(im, newp)
-            plt.title('Fit result zoomed out')
-        return newp
+    newp = zoom_out(pars, bounds)
+    if flag_p:
+        plot_fit_result(im, newp)
+        plt.title('Fit result zoomed out')
+    return newp
