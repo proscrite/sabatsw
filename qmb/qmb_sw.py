e9912ce15a5e692d28b34aba898973b3acce6f04
proscrite <pablogfr94@gmail.com>
2021-01-11 09:50:23 +0100

    Functions for evaporated mass & average QMB pulse

--- qmb/qmb_sw.py
+++ qmb/qmb_sw.py
@@ -99,7 +99,15 @@ def qmb_troughs_asALI(dfq: pd.DataFrame, dfp: pd.DataFrame, minAli: np.array, of
     return minQmb
 
 def coating_mass(dfq : pd.DataFrame, upCut: int = 10):
-    """Compute frequency shift and coating mass in QMB deposition"""
+    """Compute frequency shift and coating mass in QMB deposition
+    Input:
+    -----
+    dfq: pd.DataFrame
+        Table of QMB pulses (not necessarily aligned)
+    upCut: int
+        Upper time cut, default 10 seconds
+        (in leaky ALI  other spurious oscillations appeared)
+        """
 
     Fq = 6045000 # Blank frequency [Hz]
     Nat = 166100 # Frequency constant [Hz*cm]
@@ -128,7 +136,8 @@ def coating_mass(dfq : pd.DataFrame, upCut: int = 10):
             print(pulse + ' is too short with upCut ' + str(upCut))
             continue
 
-    return mL, deltaF
+    return mL, deltaF # Rturn mL in [g] and deltaF in [Hz]
+
 
 def plot_ali_qmb_pulses(dfp: pd.DataFrame, dfq: pd.DataFrame, ax = None, tfraction = None):
     if ax == None: ax = plt.gca()
@@ -145,4 +154,65 @@ def plot_ali_qmb_pulses(dfp: pd.DataFrame, dfq: pd.DataFrame, ax = None, tfracti
     ax2.set_yscale('log')
     ax2.tick_params(axis='y', labelcolor='r')
 
-    fig.autofmt_xdate()
+    plt.gcf().autofmt_xdate()
+
+def align_dfq(dfq: pd.DataFrame) ->pd.DataFrame:
+    """Perform time (x axis) and frequency (y axis) shifting to align QMB pulses"""
+
+    tmin = dfq['pulse0'].idxmin()
+    tshift = tmin - dfq.idxmin()
+    refPulse = tshift.idxmin()
+    taxis = dfq.index + tshift[refPulse]
+    print(refPulse, taxis)
+    dfqShift = pd.DataFrame(dfq['pulse0'])
+    dfqShift.index= list(taxis)
+    fref = dfq['pulse0'][0]
+
+    for pul in dfq.columns:
+        fshift = fref - dfq[pul][0]
+        dfqShift[pul] = pd.Series(dfq[pul].values + fshift)
+        dfqShift[pul] = dfqShift[pul].shift(-dfq[pul].idxmin())
+    return dfqShift
+
+def plotAverageQMBpulse(dfAlign: pd.DataFrame, nsigma: int = 1):
+    """Self explaining, need aligned dfq"""
+    avPulse = np.array(dfAlign.mean(axis=1))
+    sdPulse = np.array(dfAlign.std(axis=1))
+
+    color = plt.plot(dfAlign.index, avPulse)[0].get_color()
+    plt.fill_between(dfAlign.index, avPulse-nsigma*sdPulse, avPulse+nsigma*sdPulse, color=color, alpha=0.3)
+    plt.xlabel('Time [s]')
+    plt.ylabel('Frequency [Hz]')
+    plt.title('Average pulse in QMB')
+
+def evaporated_mass(dfq : pd.DataFrame, upCut: int = 10, recTime: int =10):
+    """Compute frequency recovery and solvent evaporated mass from in QMB pulse
+    """
+    Fq = 6045000 # Blank frequency [Hz]
+    Nat = 166100 # Frequency constant [Hz*cm]
+    pq = 2.649 # Single crystal quartz density [g/cm^3]
+    pf = 3.2 # Film density for Ba(ClO_4)_2 [g/cm^3]
+
+    K = Nat * pq / Fq**2   # Empirical constant [g/(cm^2 * Hz)]
+    d_qc = 0.8 # quartz crystal diameter [cm]
+    A_q = np.pi*(d_qc/2)**2  # quartz crystal area [cm^2]
+    mq = K * Fq * A_q   # Quartz crystal mass [g] (reference)
+
+    deltaF = []
+    mE = []
+    for pulse in dfq.columns:
+        try:
+            tstart = dfq[pulse].iloc[:upCut].idxmin()
+            freqsRec = dfq[pulse].iloc[tstart+2 : tstart+2 +recTime] # Frequency values during recovery
+            freqstart = dfq[pulse].iloc[tstart]  # Frequency at the valley
+            freqend = np.average(freqsRec)
+            dF = freqstart - freqend
+            deltaF.append(dF)
+
+            mE.append(mq * dF/freqstart)
+            continue
+        except ValueError:
+            print(pulse + ' is too short with upCut ' + str(upCut))
+            continue
+
+    return mE, deltaF
